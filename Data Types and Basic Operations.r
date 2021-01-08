e0f77d49030b1c9560403ea4140838b5e0a77904
arslan0302 <arslansanaullah028@gmail.com>
2021-01-08 10:22:29 +0500

    class 5.2 Attributes and Evaluation

--- Data Types and Basic Operations.r
+++ Data Types and Basic Operations.r
@@ -16,3 +16,23 @@ class(c)
 
 a = 9L
 class(a)
+
+## class 5.2 Attributes and Evaluation
+## agr hamne kisi ko value assign print karna he to 
+#1
+x <- 1
+print(x)
+## or is option ko bhi use kar skty he
+#2
+x <- 1
+x
+#3
+msg <- "Hello to the world"
+msg
+#4
+3 -> u
+u
+#4
+x <- 1:20
+x
+
